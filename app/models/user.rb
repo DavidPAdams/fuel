bd4f0010e0347115cc26a754a9dddf8a5e79d9c5
David Adams <david.riding.rails@gmail.com>
2020-03-22 12:38:29 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  before_save :downcase_email
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 40 }
@@ -7,5 +9,13 @@ class User < ApplicationRecord
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  
+  private
+
+    def downcase_email
+      email.downcase!
+    end
 
 end
