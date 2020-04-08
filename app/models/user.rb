84e23006870a476ad891041d8c5ce19f3134591d
David Adams <david.riding.rails@gmail.com>
2020-04-08 07:53:51 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,15 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :email, presence: true, 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-
+                    uniqueness: { case_sensitive: false}
   has_secure_password
   validates :password, presence: true,
-                       length: { minimum: 8 }
-
+                       length: { minimum: 8 },
+                       allow_nil: true
   
   private
 
